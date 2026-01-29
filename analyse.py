0a9f3a7be12bd4a1cd1d38e6c1a59cf0d2b3d9dc
Ronan <ronan.lhotelier@coopinnov.com>
2026-01-29 20:06:39 +0100

    Première version : calcul de moyenne et max avec gestion d'erreurs

---
+++ analyse.py
@@ -0,0 +1,18 @@
+import pandas as pd
+from calculs import calculer_max
+from calculs import calculer_moyenne
+
+def analyse(fichier):
+    try:
+        df = pd.read_csv("data.csv")
+        moyenne = calculer_moyenne(df['Prix'])
+        maximum = calculer_max(df['Prix'])
+        return moyenne, maximum
+    except:
+        return "Le fichier n'existe pas"
+    
+fichier = "data.csv"
+results = analyse(fichier)    
+print (f"Analyse terminée. Moyenne: {results[0]}, Max : {results[0]}")
+
+
---
+++ calculs.py
@@ -0,0 +1,22 @@
+def calculer_moyenne(liste_nombres):
+    """Calcule la moyenne d'une liste (C'est un docstring)"""
+    total = sum(liste_nombres)
+    resultat = division_securisee(total, len(liste_nombres))
+    return resultat
+
+def calculer_max(liste_nombre):
+    total = max(liste_nombre)
+    return total
+
+def division_securisee(a, b):
+    try:
+        resultat = a / b
+        return resultat
+    except ZeroDivisionError:
+        return "Erreur : On epeut pas diviser par zéro !"
+
+#Utilisation
+mes_donnes = [10, 20, 30, 40 ] 
+resultat = calculer_moyenne(mes_donnes)
+valeur_max = calculer_max(mes_donnes)
+print(f"La moyenne est de : {resultat} et le max est de : {valeur_max}")
